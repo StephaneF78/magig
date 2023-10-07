d638bc2590714ae65b67f60df473624400fcfe25
Stéphane Biaggini <stephane.biaggini@free.fr>
2023-10-07 18:39:38 +0200

    Magic Heart

--- main.ts
+++ main.ts
@@ -1,23 +1,9 @@
-let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-    basic.pause(200)
-    strip.showColor(neopixel.colors(NeoPixelColors.Violet))
-    basic.pause(200)
-    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.White))
-    basic.pause(1000)
-    strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    basic.pause(1000)
-})
+basic.showString("Hello!")
+basic.pause(1000)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
